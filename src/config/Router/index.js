2cffa246d7e1bca19a447e0fea37d2402461fe92
antasofa7 <arfiantasofa@gmail.com>
2021-08-08 22:55:08 +0700

    add config/router/index.js & import BrowserRouter

---
+++ src/config/Router/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+
+const Routes = () => {
+    return (
+        <Router>
+            <Switch>
+                <Routes>
+                    
+                </Routes>
+            </Switch>
+        </Router>
+    )
+}
+
+export default Routes
