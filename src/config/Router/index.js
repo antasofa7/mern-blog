42fac8ef6063eaff51a1758f01716500f52b8b37
antasofa7 <arfiantasofa@gmail.com>
2021-08-08 22:08:16 +0700

    add dir pages, components, utils, assets, & config

---
+++ src/config/Router/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+
+        </BrowserRouter>
+    )
+}
+
+export default index
