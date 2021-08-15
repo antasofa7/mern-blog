5cf78144cb67bd4921ef1ba2b00ef0e23a87ae0d
antasofa7 <arfiantasofa@gmail.com>
2021-08-15 14:22:43 +0700

    styling detail blog post page

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { LoginBG } from '../../assets';
+import './detailBlog.scss';
 
 const DetailBlog = () => {
     return (
-        <div>
-            <p>Content Detail Blog Page</p>
+        <div className="detail-blog-wrapper">
+            <img className="img-cover" src={LoginBG} alt="thumb" />
+            <p className="blog-title">Title Blog</p>
+            <p className="blog-author">Author - Date post</p>
+            <p className="blog-body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto delectus voluptate velit ad perferendis porro, impedit sit iste ipsum molestiae at ullam quo? Nam dolores eveniet aperiam assumenda nobis laboriosam.</p>
         </div>
     )
 }
